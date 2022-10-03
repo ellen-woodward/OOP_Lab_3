96fa2709ece048ecdcd5d954742b019d6522736c
Ellen Woodward <S00222321@atu.ie>
2022-10-03 10:49:36 +0100

    Add project files.

---
+++ Q1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine(IsCorrectAge(age));
+
+bool IsCorrectAge(int age)
+{
+    return (age >= 18 && age <= 21);
+}
