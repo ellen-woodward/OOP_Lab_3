dab92008d9dc8f12cb13bfe7330b26aa135660b8
Ellen Woodward <S00222321@atu.ie>
2022-10-03 11:40:56 +0100

    finding averages of a 2D array

---
+++ Q6/Program.cs
@@ -0,0 +1,25 @@
+﻿int[,] filmReviews = new int[,]
+{
+    { 3, 4, 5 },
+    { 1, 2, 1 },
+    { 5, 4, 2 }
+};
+
+double[] averages = new double[3];
+
+for (int i = 0; i < filmReviews.GetLength(0); i++)
+{
+    int total = 0;
+
+    for (int j = 0; j < filmReviews.GetLength(1); j++)
+    {
+        total += filmReviews[i, j];
+    }
+
+    averages[i] = (double)total/3;
+}
+
+foreach (double average in averages)
+{
+    Console.WriteLine($"{average:f2}");
+}
