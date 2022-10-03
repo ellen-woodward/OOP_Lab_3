1b5e8c21bb8425e06ae72d66e26c951beae6ad96
Ellen Woodward <S00222321@atu.ie>
2022-10-03 10:58:35 +0100

    switch statements

--- Q2/Program.cs
+++ Q2/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿Console.Write("Enter product : ");
+string product = Console.ReadLine().ToLower();
+
+Console.WriteLine($"Cost of {product} is {CostofProduct(product)}"); 
+
+double CostofProduct(string product)
+{
+    double cost = 0;
+
+    switch (product)
+    {
+        case "jeans":
+            cost = 67.99;
+            break;
+
+        case "jacket":
+            cost = 68.99;
+            break;
+
+        case "boots":
+            cost = 34.99;
+            break;
+
+        case "scarves":
+        case "belts":
+        case "socks":
+            cost = 10;
+            break;
+
+        default:
+            cost = -999;
+            break;
+    }
+
+    return cost;
+}
