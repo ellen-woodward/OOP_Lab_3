102f739aceb5c7ffc9c8c451050f72ee314a6994
Ellen Woodward <S00222321@atu.ie>
2022-10-03 11:22:13 +0100

    cheching stock vs sales

---
+++ Q5/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter stock-on-hand : ");
+int stock = int.Parse(Console.ReadLine());
+
+Console.Write("Enter sales : ");
+int sales = int.Parse(Console.ReadLine());
+
+if (UpdateStock(ref stock, sales))
+    Console.WriteLine($"Stock updated, stock left is {stock}");
+else
+    Console.WriteLine("Insufficiant stock");
+
+bool UpdateStock(ref int stock, int sales)
+{
+    bool isStockUpdated = false;
+
+    if (stock >= sales)
+    {
+        isStockUpdated = true;
+        stock -= sales;
+    }
+
+    return isStockUpdated;
+}
+
+
