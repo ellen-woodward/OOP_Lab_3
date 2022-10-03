7eee4ae778655cf517cf3d54ae7ccc1aabd4bdf9
Ellen Woodward <S00222321@atu.ie>
2022-10-03 11:02:11 +0100

    filling an array with zeros

---
+++ Q3/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] zeros = new int[5];
+
+Zero(zeros);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(zeros[i]);
+}
+
+void Zero(int[] zeros)
+{
+    for (int i = 0; i < zeros.Length; i++)
+    {
+        zeros[i] = 0;
+    }
+}
