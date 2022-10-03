a7c1aaa96fcc07fec648539e643dc697797fb257
Ellen Woodward <S00222321@atu.ie>
2022-10-03 11:57:41 +0100

    using lists

---
+++ Q8/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> names = new List<string>();
+
+Console.Write("Enter player name (-1 to exit): ");
+string playerName = Console.ReadLine();
+
+while (playerName != "-1")
+{
+    names.Add(playerName);
+
+    Console.Write("Enter player name (-1 to exit): ");
+    playerName = Console.ReadLine();
+}
+
+names.Sort();
+
+foreach(string name in names)
+{
+    Console.WriteLine(name);
+}
